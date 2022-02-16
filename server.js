80bea88e10d3e3271a1eba0a9c1551f59efdefa6
Patsu-rar <opatsurin@gmail.com>
2022-02-16 23:29:21 +0200

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3000; // <== You can change the port
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/testing-heroku'));
@@ -20,11 +20,7 @@ app.get('/*', (req, res) =>
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-
-
+server.listen(port);
 
