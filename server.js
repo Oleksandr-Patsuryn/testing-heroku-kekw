9a49f9ce3b9243d33c50ca58c40c40026d0aa455
Patsu-rar <opatsurin@gmail.com>
2022-02-16 23:50:19 +0200

    test

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
 const express = require('express');
-const path = require('path');
-
-const app = express();
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
@@ -11,16 +11,22 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000; // <== You can change the port
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/testing-heroku'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/testing-heroku/'}),
-);
+// app.use(express.static('./dist/testing-heroku'));
+//
+// app.get('/*', (req, res) =>
+//   res.sendFile('index.html', {root: 'dist/testing-heroku/'}),
+// );
 
 server.use(middlewares);
 server.use(router);
-app.use(server.listen(port));
+server.use(express.static('./dist/testing-heroku'));
+server.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/testing-heroku/'}),
+);
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port);
+// app.use(server.listen(port));
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 
