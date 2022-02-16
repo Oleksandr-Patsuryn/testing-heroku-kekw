d37a6f206fce712997ad4c101868a535ccf09bf9
Patsu-rar <opatsurin@gmail.com>
2022-02-16 23:16:09 +0200

    test

--- server.js
+++ server.js
@@ -1,18 +1,4 @@
-//Install express server
-const express = require('express');
-const path = require('path');
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/testing-heroku'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/testing-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
