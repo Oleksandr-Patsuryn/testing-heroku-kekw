ad17c52b5836eb5446dd1d55647b03a6b5fc66c7
Patsu-rar <opatsurin@gmail.com>
2022-02-16 23:11:09 +0200

    test

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
